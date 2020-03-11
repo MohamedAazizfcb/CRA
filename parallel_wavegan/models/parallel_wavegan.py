92032ccad295e1c455e323bfdbbb655f3dbfe219
Dalia <dalia.easa98@eng-st.cu.edu.eg>
2020-03-11 14:09:30 +0200

    start models & layers

---
+++ parallel_wavegan/models/parallel_wavegan.py
@@ -0,0 +1,33 @@
+"""Parallel WaveGAN Modules."""
+
+import logging
+import math
+
+import torch
+
+from parallel_wavegan.layers import Conv1d
+from parallel_wavegan.layers import Conv1d1x1
+from parallel_wavegan.layers import ResidualBlock
+from parallel_wavegan.layers import upsample
+from parallel_wavegan import models
+
+
+class ParallelWaveGANGenerator(torch.nn.Module):
+    """Parallel WaveGAN Generator module."""
+
+# TODO YOUSSRY
+
+
+
+
+
+
+
+
+
+class ParallelWaveGANDiscriminator(torch.nn.Module):
+    """Parallel WaveGAN Discriminator module."""
+
+
+# TODO DALIA
+
