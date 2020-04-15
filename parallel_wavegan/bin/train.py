1502287d685b0f901303a2f10e4be2a9a7b46953
MohamedAazizfcb <37511776+MohamedAazizfcb@users.noreply.github.com>
2020-04-15 21:22:58 +0200

    add saving and loading checkpoints in train.py

--- parallel_wavegan/bin/train.py
+++ parallel_wavegan/bin/train.py
@@ -76,3 +76,123 @@ from parallel_wavegan.utils import read_hdf5 #Utility functions.
 
 
 matplotlib.use("Agg") #To avoid matplotlib error in command-line interface (CLI) environment
+
+
+class Trainer(object):
+    """this module is used to train parallel wavegan"""
+
+    def __init__(self,
+                 steps,                         #(int): Initial number of global steps.
+                 epochs,                        #(int): Initial number of global epochs.
+                 data_loader,                   #(dict): Dictionary of data loaders (contrains "train" and "dev" loaders).
+                 model,                         #(dict): Dictionary of models (contains "generator" and "discriminator" models).
+                 criterion,                     #(dict): Dictionary of criterions (contrains "stft" and "mse" criterions).
+                 optimizer,                     #(dict): Dictionary of optimizers (contrains "generator" and "discriminator" optimizers).
+                 scheduler,                     #(dict): Dictionary of schedulers (contrains "generator" and "discriminator" schedulers).
+                 config,                        #(dict): Config dictionary loaded from yaml format configuration file.
+                 device=torch.device("cpu"),    #(torch.deive): Pytorch device instance.
+                 ):
+        
+        #initialzations:
+        self.steps = steps
+        self.epochs = epochs
+        self.data_loader = data_loader
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.config = config
+        self.device = device
+
+        self.writer = SummaryWriter(config["outdir"]) #to write the events while training asynchronously.
+        self.finish_train = False # true when training is finished
+        self.total_train_loss = defaultdict(float) # the stft loss function results on training phase 
+        self.total_eval_loss = defaultdict(float) # the stft loss function results on eavaluating phase 
+
+    def run(self):
+        """Run training epoch by epoch."""
+        self.tqdm = tqdm(initial=self.steps,
+                         total=self.config["train_max_steps"],
+                         desc="[train]") # progress bar of steps done from maximum number of steps
+
+        while not self.finish_train: # loop till the training is finished
+            self._train_epoch() #training of one epoch 
+
+        self.tqdm.close() # close the progress bar when training is finished
+        logging.info("Training is complete!, Please Be Happy :)!") 
+
+
+
+    def save_checkpoint(self, checkpoint_path):
+        """Save checkpoint of comlpeted training results."""
+
+        """
+        Arguments:
+            checkpoint_path (str): the path inwhich we save Checkpoint.
+        """
+
+        ####################################
+        """
+        we make a state dictionary that has the following archetecture:
+            optimizer{state dictionary of the generator, state dictionary of the discriminator},
+            scheduler{state dictionary of the generator, state dictionary of the discriminator},
+            steps
+            epochs
+            model{state dictionary of the generator, state dictionary of the discriminator},
+        """
+        ####################################
+
+        state_dict = { 
+            "optimizer": {
+                "generator": self.optimizer["generator"].state_dict(),
+                "discriminator": self.optimizer["discriminator"].state_dict(),
+            },
+            "scheduler": {
+                "generator": self.scheduler["generator"].state_dict(),
+                "discriminator": self.scheduler["discriminator"].state_dict(),
+            },
+            "steps": self.steps,
+            "epochs": self.epochs,
+        }
+
+        if self.config["distributed"]:# if we confiured distributed
+            state_dict["model"] = {
+                "generator": self.model["generator"].module.state_dict(), # get the state of the module 
+                "discriminator": self.model["discriminator"].module.state_dict(),# get the state of the module
+            }
+        else:# if we confiured NOT distributed
+            state_dict["model"] = {
+                "generator": self.model["generator"].state_dict(), #get the state of the whole model
+                "discriminator": self.model["discriminator"].state_dict(),#get the state of the whole model
+            }
+
+        if not os.path.exists(os.path.dirname(checkpoint_path)): #if the checkpoint_path is not found
+            os.makedirs(os.path.dirname(checkpoint_path))   #make the path
+        torch.save(state_dict, checkpoint_path)  # save the state dictionary in the checkpoint path
+
+
+    def load_checkpoint(self, checkpoint_path, load_only_params=False):
+        """Load a checkpoint to start from it."""
+        
+        """
+        Args:
+            checkpoint_path (str): Checkpoint path to be loaded.
+            load_only_params (bool): Whether to load only model parameters.
+        """
+
+        state_dict = torch.load(checkpoint_path, map_location="cpu") #load the checkpoint to the cpu
+        if self.config["distributed"]: # if we confiured distributed
+            self.model["generator"].module.load_state_dict(state_dict["model"]["generator"]) #load the state of the generator module of the model
+            self.model["discriminator"].module.load_state_dict(state_dict["model"]["discriminator"]) #load the state of the discriminator module of the model
+        else:# if we confiured NOT distributed
+            self.model["generator"].load_state_dict(state_dict["model"]["generator"])#load the state of the generator model
+            self.model["discriminator"].load_state_dict(state_dict["model"]["discriminator"])#load the state of the discriminator model
+        if not load_only_params:
+            self.steps = state_dict["steps"] #load number of steps
+            self.epochs = state_dict["epochs"]#load number of epochs
+            self.optimizer["generator"].load_state_dict(state_dict["optimizer"]["generator"])#load the optimizer generator state
+            self.optimizer["discriminator"].load_state_dict(state_dict["optimizer"]["discriminator"])#load the optimizer discriminator state
+            state_dict["scheduler"]["generator"].update(**self.config["generator_scheduler_params"])#overwrite schedular generator argument parameters
+            state_dict["scheduler"]["discriminator"].update(**self.config["discriminator_scheduler_params"])#overwrite schedular discriminator argument parameters
+            self.scheduler["generator"].load_state_dict(state_dict["scheduler"]["generator"])#load the scheduler generator state
+            self.scheduler["discriminator"].load_state_dict(state_dict["scheduler"]["discriminator"])#load the scheduler discriminator state
